54aba16c5f7469dfcaff8e718e13aad660b9719e
Selahi KORKUT <37619156+selahikorkut@users.noreply.github.com>
2022-06-06 12:27:03 +0300

    Add files via upload

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni at ad voluptas, repellendus nostrum velit fugit harum minus. Ducimus, id.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
